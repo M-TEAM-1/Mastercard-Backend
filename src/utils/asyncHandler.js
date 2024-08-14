e32acf2e3269448ab1432407b913a0208a35c75f
Swastick Das <swastickdas95@gmail.com>
2024-08-14 13:04:26 +0530

    Changed the AsyncHandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,14 +1,11 @@
 //This is actually a wrapper function that I have made in order to execute the async await function for connecting the database swiftly.
-export {asyncHandler}
-
-
-const asyncHandler =(fn) => async (req,res,next)=>{ // This is actually a wrapper function
-    try{
-        await fn(req,res,next)
-    } catch(error){
-        res.status(err.code || 500).json({
-            succes: false,
-            message: err.message
-        })
+const asyncHandler=(requestHandler) =>{
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
     }
 }
+
+
+
+
+export {asyncHandler}
